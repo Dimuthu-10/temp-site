df37af34b1ad5b55f01ceee3d2d17b083c517a3e
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-08-30 09:35:22 +0530

    add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React World</h1>
-      <p>This is the Body of App component</p>
+      <Home />
     </div>
   );
 }
