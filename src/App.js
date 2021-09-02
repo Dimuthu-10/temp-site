0265c8d6c0c50ca1432b89921740438a26d50a80
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-09-02 11:40:48 +0530

    add router and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Home from './Pages/Home';
 import Search from './Pages/Search';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Search />
+      <Router>
+        <Switch>
+
+          {/* Search Route */}
+          <Route path='/search'>
+            <Search />
+          </Route>
+
+          {/* Home Route */}
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
