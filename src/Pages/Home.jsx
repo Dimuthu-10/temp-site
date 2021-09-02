d0ac54b9875b6cf2a1f379071819d3b886ae7da5
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2021-09-02 14:43:16 +0530

    create footer links

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,43 @@
 import React from 'react';
-import "./Home.css";
+import './Home.css'
 import { Link } from 'react-router-dom';
 
 const Home = () => {
     return ( 
-        <div>
-            <h1>This is a home page</h1>
-            
-            {/* Header bar including nav */}
-            <div className="homepage_Header">
-                <nav className="homepage_nav">
-                    <Link to="gmail">Gmail</Link>
-                    <Link to="images">Images</Link>
-                </nav>
-            </div>
+            <div className="home">
+                        
+                {/* Header bar including nav */}
+                <div className="homepage_Header">
+                    <nav className="homepage_nav">
+                        <Link to="gmail">Gmail</Link>
+                        <Link to="images">Images</Link>
+                    </nav>
+                </div>
 
-            {/* content */}
-            <div className="homepage_body">
-                <p>This is Body</p>
-            </div>
+                {/* content */}
+                <div className="homepage_body">
+                    <p>This is Body</p>
+                </div>
 
-            {/* Footer */}
-            <div className="homepage_Footer">
-                this is Footer
+                {/* Footer */}
+                <div className="homepage_Footer">
+                    <nav className='footer_nav'>
+
+                        <div className="nav_left">
+                            <Link to="About">About</Link>
+                            <Link to="Advertising">Advertising</Link>
+                            <Link to="Business">Business</Link>
+                            <Link to="How_Search_Works">How Search Works</Link>
+                        </div>
+
+                        <div className="nav_right">
+                            <Link to="privacy">Privacy</Link>
+                            <Link to="terms">Terms</Link>
+                            <Link to="settings">Settings</Link>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
      );
 }
  
